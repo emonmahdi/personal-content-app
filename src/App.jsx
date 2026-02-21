968169ea33da0d9f31a03c3a42eae5820a44374d
emonmahdi <developeremon1997@gmail.com>
2026-02-21 22:04:34 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+      <h2 className='text-3xl font-bold underline bg-purple-700 p-4'>Personal Content App</h2>
+    </>
+  )
+}
+
+export default App
